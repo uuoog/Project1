f71e63ec6d690e0974f4107eaa53058785958a99
uuoog <d2994@naver.com>
2023-05-09 14:41:08 +0900

    커밋 1: main.py를 생성하고 더하기 알고리즘을 구현함

---
+++ main.py
@@ -0,0 +1,11 @@
+## 함수
+def add_func(n1, n2):
+    result = n1 + n2
+    return result
+
+## 전역변수
+num1, num2, res = 100, 200, 0
+
+## 메인코드
+res = add_func(num1, num2)
+print(num1, '+' , num2, '=', res)
